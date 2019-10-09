d1068c4f321789c78fb2d1e43696e02d7946b401
Boris <boris.bdc@gmail.com>
2019-10-09 09:37:43 -0300

    primera carga del proyecto, el mismo consta de pagina inicial desarrollada en laravel.

---
+++ resources/views/index.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app', [
+'jumboTitle' => 'Bienvenido a MBanking, que querés hacer hoy?',
+'jumboDesc' => 'En este sitio podes operar con tu cuenta mirando el balance, haciendo transferencias, pagando servicios y armando inversiones!'
+])
+
+@section('content')
+
+
+<div class="container">
+    <div class="row" style="padding: 2%">
+        <div class="col col-margin">
+            <div class="card text-center">
+                <i class="fas fa-university img-index" alt="balance"></i>
+                <div class="card-body">
+                    <h5 class="card-title">Balance</h5>
+                    <p class="card-text">Controle sus gastos y proyecte sus futuros movimientos.</p>
+                    <a href="balance.php" class="btn btn-secondary btnHome">Ver Balance</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col col-margin">
+            <div class="card text-center">
+                <i class="fas fa-money-check-alt img-index" alt="pago de servicios"></i>
+                <div class="card-body">
+                    <h5 class="card-title">Pago de Servicios</h5>
+                    <p class="card-text">Paga todo lo que necesites desde donde estes.</p>
+                    <a href="servicios.php" class="btn btn-secondary btnHome">Pagar Servicios</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col col-margin">
+            <div class="card text-center">
+                <i class="fas fa-chart-bar img-index" alt="inversiones"></i>
+                <div class="card-body">
+                    <h5 class="card-title">Inversiones</h5>
+                    <p class="card-text">Aumenta tus ahorros en el circuito financiero.</p>
+                    <a href="inversiones.php" class="btn btn-secondary btnHome">Invertir</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/7005a1e6a6.js"></script>
+    <link rel="stylesheet" href="{{ URL::asset('fonts/minipax/minipax_bold_macroman/stylesheet.css')}}">
+    <link rel="stylesheet" href="{{ URL::asset('fonts/minipax/minipax_regular_macroman/stylesheet.css')}}">
+    <link rel="stylesheet" href="{{ URL::asset('fonts/raleway-v14-latin/stylesheet.css')}}">
+    <link rel="stylesheet" href="{{ URL::asset('css/executive.css')}}">
+    <link rel="stylesheet" href="{{ URL::asset('css/style.css')}}">
+    <title>Home Banking</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <i class="fas fa-coins navbar-brand fa-color" href="#"></i>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="{!! url('/') !!}">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{!! url('/balances') !!}">Balance</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{!! url('/services') !!}">Pago de Servicios</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{!! url('/investments') !!}">Inversiones</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h1 class="display-4 raleFont" style='font-weight: bold'>{{ $jumboTitle }}</h1>
+            <p class="lead raleFont">{{ $jumboDesc }}</p>
+        </div>
+    </div>
+
+    @yield('content')
+
+    <footer>
+        <div class="row">
+            <div class="col">
+                <div class="pie bg-dark" style='padding-top: 1%'>
+                    <p class="textPie">Todos los derechos reservados a <em>Elefante de Caja</em> ©.</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+    @yield('scripts')
+</body>
+
+</html>
